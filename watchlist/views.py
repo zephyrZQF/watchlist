03e08b5e93b1e19eaf501281d51e85796a8a33f2
zephyrZQF <527941789@qq.com>
2019-12-09 08:44:03 +0800

    全局

--- watchlist/views.py
+++ watchlist/views.py
@@ -8,9 +8,8 @@ from flask import render_template,url_for
 @app.route('/index')
 @app.route('/home')
 def index():
-    user = User.query.first()
     movies = Movie.query.all()
-    return render_template('index.html',user=user,movies=movies)
+    return render_template('index.html',movies=movies)
 
 @app.route('/user/<name>')
 def user_page(name):
@@ -26,5 +25,9 @@ def test_url_for():
 
 @app.errorhandler(404)
 def page_not_found(e):
+    return render_template('404.html'),404
+
+@app.context_processor
+def inject_user():
     user = User.query.first()
-    return render_template('404.html',user=user),404
+    return dict(user=user)
