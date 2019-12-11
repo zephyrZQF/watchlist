65b8db87cbfaf5924ca6774568eab0b143b8af8b
zephyrZQF <527941789@qq.com>
2019-12-11 19:24:31 +0800

    flash

--- watchlist/__init__.py
+++ watchlist/__init__.py
@@ -8,6 +8,8 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(app.root_path,'data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # 关闭对模型修改的监控
+app.config['SECRET_KEY'] = 'dev'
+
 db = SQLAlchemy(app)
 
 from watchlist import views,models,commands
